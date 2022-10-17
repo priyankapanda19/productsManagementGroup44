9c8bfdb324def5178dba875fca555a48a16d68c0
Tanweer Zaya <104303869+zayatanweer@users.noreply.github.com>
2022-10-17 12:55:23 +0530

    cart & ordel model

---
+++ src/models/cartModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+
+    userId: {
+        type: ObjectId,
+        ref: "User",
+        required: true,
+        unique: true
+    },
+    items: [{
+        productId:{
+            type: ObjectId,
+            ref: "Product",
+            required: true
+        },
+        quantity:{
+            type: Number,
+            required: true
+        } 
+    }],
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    totalItems: {
+        type: Number,
+        required: true
+    },
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("Cart", cartSchema)
---
+++ src/models/orderModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const orderSchema = new mongoose.Schema({
+    userId:{
+        type:ObjectId,
+        refs:"User",
+        required:true
+    },
+    
+    items:[{
+        productId:{
+        type:ObjectId,
+        refs:"Product",
+        required:true
+    },
+    quantity:{
+        type:Number,
+        required:true
+    }}],
+    totalPrice:{
+        type:Number,
+        required:true
+    },
+    totalItems:{
+        type:Number,
+        required:true
+    },
+    totalQuantity:{
+        type:Number,
+        required:true
+    },
+    cancellable:{
+        type:Boolean,
+        default:true
+    },
+    status:{
+        type:String,
+        enum:["pending", "completed", "cancled"],
+        default:'pending'
+    },
+    deletedAt:{
+        type:Date,
+        default:null
+    },
+    isdeleted:{
+        type:Boolean,
+        default:false
+    }
+    
+},{timestamps:true})
+
+module.exports = mongoose.model('Order',orderSchema)
